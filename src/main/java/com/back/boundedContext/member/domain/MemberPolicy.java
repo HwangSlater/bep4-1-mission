e827770fd8611bcd7b0c847f5a20574ef2850154
HwangSlater <doorchan23@naver.com>
2025-12-24 10:23:59 +0900

    0010

---
+++ src/main/java/com/back/boundedContext/member/domain/MemberPolicy.java
@@ -0,0 +1,29 @@
+package com.back.boundedContext.member.domain;
+
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+@Service
+public class MemberPolicy {
+    private static final int PASSWORD_CHANGE_DAYS = 90;
+
+    public Duration getNeedToChangePasswordDuration() {
+        return Duration.ofDays(PASSWORD_CHANGE_DAYS);
+    }
+
+    public int getNeedToChangePasswordDays() {
+        return PASSWORD_CHANGE_DAYS;
+    }
+
+    public boolean isNeedToChangePassword(LocalDateTime lastChangedAt) {
+        if(lastChangedAt == null) {
+            return true;
+        }
+
+        return lastChangedAt
+                .plusDays(PASSWORD_CHANGE_DAYS)
+                .isBefore(LocalDateTime.now());
+    }
+}
