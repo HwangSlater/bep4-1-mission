625014fb82607ff8f21fa3026b30efde368293f2
HwangSlater <doorchan23@naver.com>
2025-12-26 16:03:48 +0900

    0028

--- src/main/java/com/back/boundedContext/post/app/PostFacade.java
+++ src/main/java/com/back/boundedContext/post/app/PostFacade.java
@@ -2,8 +2,6 @@ package com.back.boundedContext.post.app;
 
 import com.back.boundedContext.post.domain.Post;
 import com.back.boundedContext.post.domain.PostMember;
-import com.back.boundedContext.post.out.PostMemberRepository;
-import com.back.boundedContext.post.out.PostRepository;
 import com.back.global.RsData.RsData;
 import com.back.shared.member.dto.MemberDto;
 import lombok.RequiredArgsConstructor;
@@ -15,13 +13,13 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class PostFacade {
-    private final PostRepository postRepository;
-    private final PostMemberRepository postMemberRepository;
     private final PostWriteUseCase postWriteUseCase;
+    private final PostSyncMemberUseCase postSyncMemberUseCase;
+    private final PostSupport postSupport;
 
     @Transactional(readOnly = true)
     public long count() {
-        return postRepository.count();
+        return postSupport.count();
     }
 
     @Transactional
@@ -31,26 +29,17 @@ public class PostFacade {
 
     @Transactional(readOnly = true)
     public Optional<Post> findWalletById(int id) {
-        return postRepository.findById(id);
+        return postSupport.findWalletById(id);
     }
 
     @Transactional
     public PostMember syncMember(MemberDto member) {
-        PostMember postMember = new PostMember(
-                member.id(),
-                member.createDate(),
-                member.modifyDate(),
-                member.username(),
-                "",
-                member.nickname(),
-                member.activityScore()
-        );
-
-        return postMemberRepository.save(postMember);
+        return postSyncMemberUseCase.syncMember(member);
     }
+
 
     @Transactional(readOnly = true)
     public Optional<PostMember> findMemberByUsername(String username) {
-        return postMemberRepository.findByUsername(username);
+        return postSupport.findMemberByUsername(username);
     }
 }
